bd4ffed2ac6c23293768b34df7c1ffbe07b1eb83
Yabut, Leandro (NIH/OD) [C] <leandro.yabut@nih.gov>
2021-10-29 23:44:12 -0600

    Add about dialog.

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,21 +1,39 @@
 import React from "react";
 import Swal from "sweetalert2";
 import infoIcon from '../assets/info.svg';
-import 'animate.css';
+import '@sweetalert2/theme-borderless';
+import './Info.css';
 
 const Info = () => {
 
     const openInfo = () => {
         Swal.fire({
-            iconHtml: '<img src="icon.png" width="175px"/>',
+            iconHtml: '<img src="icon.png" width="175px" class="icon-image"/>',
             iconColor: 'transparent',
             titleText: 'About Moody Weather',
-            html: `<p style='font-size: 1rem; text-align: left'>
-            The weather seems a little moody. 
-            Bad weather? Get insulted. Good weather? 
-            Get complimented. Make your bad days worse 
-            and your good days better.</p>`,
-            confirmButtonColor: '#4982c2'
+            html: `
+                <p style='font-size: 1rem; text-align: left'>
+                    The weather seems a little moody. 
+                    Bad weather? Get insulted. Good weather? 
+                    Get complimented. Make your bad days worse 
+                    and your good days better.
+                </p>
+                
+                <div style="font-size: 0.7rem; text-align: left;">
+                    Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a>
+                    <br/>
+                    Insults by <a href="https://evilinsults.com/" title="Evil Insult Generator">Evilinsults.com</a>
+                    <br/>
+                    Compliments by <a href="https://complimentr.com/" title="Complimentr">Complimentr.com</a>
+                </div>
+                `,
+            confirmButtonColor: '#4982c2',
+            showClass: {
+                popup: 'animate__animated animate__fadeIn animate__faster'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOut animate__faster',
+            }
         });
     };
 
--- src/components/MoodyWeather.tsx
+++ src/components/MoodyWeather.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Loader from "./Loader";
 import { usePosition } from "use-position";
 import "./MoodyWeather.css";
-import Loader from "./Loader";
+import "animate.css";
 
 type MoodyWeatherModel = {
     weather:  {
@@ -31,65 +32,95 @@ type MoodyWeatherModel = {
 const MoodyWeather = ({tempUnit, degradeMode}: { tempUnit: 'f' | 'c', degradeMode: boolean }) => {
 
     const [moodyWeather, setMoodyWeather] = useState<MoodyWeatherModel | undefined | null>();
+    const [loading, setLoading] = useState(false);
     const { latitude, longitude, errorMessage } = usePosition(false);
     
-    const loadMoodyWeather = (latitude: number, longitude: number, errorMessage: string) => {
-        let q: string | undefined;
-
-        if (latitude && longitude && !errorMessage) {
-            q = `${latitude},${longitude}`;
-        }
-
-        axios.get(`${process.env.REACT_APP_API}/moody-weather`, {
-            params: { q, degrading: degradeMode }
-        }).then(response => {
-            setMoodyWeather(response.data);
-        });
-    };
-
-    useEffect(() => loadMoodyWeather(latitude!, longitude!, errorMessage!), [latitude, longitude, errorMessage]);
+    useEffect(() => {
+        setLoading(true);
+        const loadMoodyWeather = (latitude: number, longitude: number, errorMessage: string) => {
+            let q: string | undefined;
+    
+            if (latitude && longitude && !errorMessage) {
+                q = `${latitude},${longitude}`;
+            }
+    
+            axios.get(`${process.env.REACT_APP_API}/moody-weather`, {
+                params: { q, degrading: degradeMode }
+            }).then(response => {
+                setMoodyWeather(response.data);
+                setLoading(false);
+            });
+        };
+        loadMoodyWeather(latitude!, longitude!, errorMessage!);
+    }, 
+    [latitude, longitude, errorMessage, degradeMode]);
 
     return (
         <div>
             {moodyWeather ? 
             <>
-            <div className="weather-card">
-                <div className="condition-info">
-                    <div className="condition-summary">
-                        <div className="condition-temp">
-                            {
-                                tempUnit === 'f' ? 
-                                    moodyWeather?.weather.current.temp_f :
-                                    moodyWeather?.weather.current.temp_f
-                            }&deg;
-                        </div>
-                        <div className="condition-icon-container">
-                            <div className="condition-icon">
-                                <img src={moodyWeather?.weather.current.condition.icon} 
-                                    alt={moodyWeather?.weather.current.condition.text}/>
-                            </div>
-                            <div className="condition-text">
-                                {moodyWeather?.weather.current.condition.text}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="location-info">
-                    <div className="location-name">
-                        {moodyWeather?.weather.location.name}
-                    </div>
-                    <div className="location-sub">
-                        {moodyWeather?.weather.location.region}, {moodyWeather?.weather.location.country}
-                    </div>
-                </div>
-            </div>
-            <div className="speech-bubble">
-                {moodyWeather?.dialogue}
-            </div>
-            </> : <Loader></Loader>}
+                <MoodyWeatherCard moodyWeather={moodyWeather} tempUnit={tempUnit}/> 
+                {
+                    loading ? 
+                    <div style={{
+                        width: "100vw",
+                        height: "100vh",
+                        display: "flex",
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        position: "absolute",
+                        top: 0,
+                        left: 0
+                    }}>
+                        <Loader/>
+                    </div> : null
+                }
+            </> 
+            : <Loader/>}
         </div>
     );
 
+};
+
+export const MoodyWeatherCard = ({moodyWeather, tempUnit}:
+    {
+        moodyWeather: MoodyWeatherModel,
+        tempUnit: 'f' | 'c'
+    }) => {
+    return (<><div className="weather-card">
+        <div className="condition-info">
+            <div className="condition-summary">
+                <div className="condition-temp">
+                    {
+                        tempUnit === 'f' ? 
+                            moodyWeather.weather.current.temp_f :
+                            moodyWeather.weather.current.temp_f
+                    }&deg;
+                </div>
+                <div className="condition-icon-container">
+                    <div className="condition-icon">
+                        <img src={moodyWeather.weather.current.condition.icon} 
+                            alt={moodyWeather.weather.current.condition.text}/>
+                    </div>
+                    <div className="condition-text">
+                        {moodyWeather.weather.current.condition.text}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="location-info">
+            <div className="location-name">
+                {moodyWeather.weather.location.name}
+            </div>
+            <div className="location-sub">
+                {moodyWeather.weather.location.region}, {moodyWeather.weather.location.country}
+            </div>
+        </div>
+    </div>
+    <div className="speech-bubble animate__animated animate__bounceIn aniamte__faster">
+        {moodyWeather.dialogue}
+    </div></>);
 };
 
 export default MoodyWeather;
