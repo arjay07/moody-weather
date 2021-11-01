61630b6ef9b576f40def2a5e23bf45c0278001cd
leandroyabut <leandro.yabut@smoothstack.com>
2021-11-01 00:20:41 -0600

    Remove borderless them for Swal.

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Swal from "sweetalert2";
 import infoIcon from '../assets/info.svg';
-import '@sweetalert2/theme-borderless';
 import './Info.css';
 
 const Info = () => {
